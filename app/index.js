119b3d26a559e7a655434ef536f7f60c09a6b8b8
Simeon Theobald <simeon.theobald@gmail.com>
2016-07-03 18:36:15 +1200

    configureStore and browserHistory from ./store
    Enabling routerMiddleware and DRY browserHistory

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
-import configureStore from './store/configureStore';
+import { configureStore, browserHistory } from './store/configureStore';
 import Root from './containers/Root';
 import './styles/main.scss';
 
